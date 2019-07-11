1eca22522a05575267e630a86222a2343b8d7a3f
jimmy wallace <xeekatar@gmail.com>
2019-07-11 09:26:23 -0400

    moved all js into one file

--- dice_roller.js
+++
@@ -1,9 +0,0 @@
-function roll_dice(){
-    var num_dice = parseInt(document.getElementById("num").value);
-    var die_type = parseInt(document.getElementById("dieType").value);  
-    var modifier = parseInt(document.getElementById("modifier").value);
-    
-    var roll = num_dice * Math.floor(Math.random() * die_type) + modifier + 1; 
-
-    document.getElementById('result').innerHTML = roll;
-}
